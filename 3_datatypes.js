d748c43e6c8c0e40f19aa82d4bbaadc05706cb54
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-12-10 18:11:24 +0000

    discussion datatypes

--- 3_datatypes.js
+++ 3_datatypes.js
@@ -13,6 +13,9 @@ let name = "parul"
 let age = 18
 let isLoggedIn = false
 let state;
+console.log(typeof "parul");
+
+// primitive datatypes
 
 // number => 2 to power 53
 // bigint
@@ -23,7 +26,19 @@ let state;
 // symbol => unique
 
 
-// object
+// object 
+/* non-primitive data type object in js can be created as follows: */
+
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+let x = "Parul Sharma";
+let y = 22;
+let z = 3.55
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
