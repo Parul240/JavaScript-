8a9d0ead51d03bcec02a830a7d7d0f43e314a2a3
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2023-12-12 17:42:20 +0000

    discuss comparisions

--- 5_conditionalOperators.js
+++ 5_conditionalOperators.js
@@ -56,8 +56,7 @@ console.log(undefined < 0);
 console.log(undefined > 0);
 
 /*
-the reason is that an equality check == and comparisons 
-<, 
->=,
-<=
+The reason is that an equality check == and comparisons >, <, >=, <= work diffrently.
+Comparisons convert null to a number, treating it as 0
+That's why (3) null >= 0 is true and (1) null > 0 is false.
 */
