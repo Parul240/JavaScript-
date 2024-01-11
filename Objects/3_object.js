3f3db102ca882a4460bee0f5b9ceba6d31e5cb54
Parul Sharma <114149620+Parul240@users.noreply.github.com>
2024-01-11 17:50:32 +0530

    object de-structure & JSON API

---
+++ Objects/3_object.js
@@ -0,0 +1,24 @@
+const course = {
+  coursename: "js tutorial",
+  price: "999",
+  courseInstructor: "parul",
+};
+
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "parul",
+//     "coursename": "js tutorial",
+//     "price": "free"
+// }
+
+[
+  {}, 
+  {}, 
+  {}
+];
